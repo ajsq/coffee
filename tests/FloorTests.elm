9fe92fa12bb04fb3e6f17407d09b3c7adb3f4585
Amy Wilhelm <35502109+ajsq@users.noreply.github.com>
2019-08-19 11:25:53 -0700

    Nothing to actually test in elm atm. hm.

--- tests/FloorTests.elm
+++ tests/FloorTests.elm
@@ -1,57 +1,10 @@
-module FloorTests exposing (dupe, verifyDupeTest)
+module FloorTests exposing (noRealTest)
 
 import Expect exposing (Expectation)
-import Floors exposing (floorList)
 import Test exposing (Test, describe, test)
 
 
-dupe : Test
-dupe =
-    test "unique floors"
-        (\_ ->
-            List.map .floor floorList
-                |> hasDupes
-                |> Expect.false "Found dupes in the list"
-        )
-
-
-verifyDupeTest : Test
-verifyDupeTest =
-    describe "Sanity check hasDupes"
-        [ test "no dupes"
-            (\_ ->
-                hasDupes [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-                    |> Expect.false "Found dupe where there is none"
-            )
-        , test "dupes in order"
-            (\_ ->
-                hasDupes [ 1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9, 10 ]
-                    |> Expect.true "Missed dupe in order"
-            )
-        , test "dupes out of order"
-            (\_ ->
-                hasDupes [ 1, 2, 3, 4, 5, 6, 3, 7, 4, 8, 9, 10 ]
-                    |> Expect.true "Missed dupe out of order"
-            )
-        ]
-
-
-hasDupes : List comparable -> Bool
-hasDupes aList =
-    List.sort aList
-        |> List.foldl checkDupItem ( Nothing, False )
-        |> Tuple.second
-
-
-checkDupItem : comparable -> ( Maybe comparable, Bool ) -> ( Maybe comparable, Bool )
-checkDupItem a ( maybePrev, seenDup ) =
-    if seenDup == True then
-        ( Just a, True )
-
-    else
-        case maybePrev of
-            Nothing ->
-                ( Just a, False )
-
-            Just prev ->
-                ( Just a, a == prev )
+noRealTest : Test
+noRealTest =
+    test "not doing any actual testing right now" <|
+        \_ -> Expect.equal 1 1
